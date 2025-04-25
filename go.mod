67298fd7867930caefb4bbd396045a18a9603dd7
Mounir Ybane <rinuom91@gmail.com>
2025-04-25 20:39:43 +0800

    Integrated mdns.

--- go.mod
+++ go.mod
@@ -3,18 +3,19 @@ module lumbay2sv
 go 1.24.1
 
 require (
+	github.com/joho/godotenv v1.5.1
 	google.golang.org/grpc v1.72.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
-	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
-	github.com/grandcat/zeroconf v1.0.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/miekg/dns v1.1.27 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
+	github.com/hashicorp/mdns v1.0.6
+	github.com/miekg/dns v1.1.55 // indirect
+	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
 )
--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
+	"github.com/hashicorp/mdns"
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
@@ -22,6 +24,31 @@ func main() {
 	if len(port) == 0 {
 		port = "50052"
 	}
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		portInt = 50052
+	}
+
+	serviceName := os.Getenv("LUMBAY2_SERVER_SERVICE_NAME")
+	if len(serviceName) == 0 {
+		serviceName = "_lumbay._tcp"
+	}
+
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("unable to get hostname: %v\n", err)
+	}
+
+	mdnsService, err := mdns.NewMDNSService(host, serviceName, "", "", portInt, nil, nil)
+	if err != nil {
+		log.Fatalf("unable to create mDNS service: %v\n", err)
+	}
+
+	mdnsServer, err := mdns.NewServer(&mdns.Config{Zone: mdnsService})
+	if err != nil {
+		log.Fatalf("unable to create mDNS server: %v\n", err)
+	}
+	defer mdnsServer.Shutdown()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
